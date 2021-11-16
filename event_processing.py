b24a72d91aedd571e016e669d6f48400a5247b1b
Айназик <phystech.aa@mail.ru>
2021-11-16 22:02:02 +0300

    Добавлены условия ограничения поднятия на этажи(до 5) и опускания(до 1). Добавлены ограничения на перемещение по оси х. Управление D - вправо A - влево F - входить/выходить из лифта "ВНИЗ" - лифт опускает человека на этаж вниз "ВВЕРХ" - лифт поднимает человека на этаж вверх

--- event_processing.py
+++ event_processing.py
@@ -14,10 +14,10 @@ def process_event(event, hero_coordinates):
         game_end = True
     # обработка событий с клавиатуры
     if event.type == pygame.KEYDOWN:
-        # движение по главной плоскости xOy вдоль x
-        if event.key == pygame.K_a:
+        # движение по главной плоскости xOz вдоль x
+        if event.key == pygame.K_a and hero_coordinates[0] != 0:
             hero_coordinates[0] -= 1
-        elif event.key == pygame.K_d:
+        elif event.key == pygame.K_d and hero_coordinates[0] != 20:
             hero_coordinates[0] += 1
         # лифт (необходимо добавить условие нахождения лифта в ячеке нахождения перс.
         elif event.key == pygame.K_F:
@@ -28,11 +28,10 @@ def process_event(event, hero_coordinates):
                 # механизм запускающий отрисовку вхождения в лифт (например отдельный параметр True or False,
                 # который передается в раздел отрисовки)
                 pass
-        # опускается на один этаж, меняется координата по y
-        elif event.key == pygame.K_DOWN and hero_coordinates[3] == True:
-            hero_coordinates[1] -= 1
-        # поднимается на один этаж, меняется координата по y
-        elif event.key == pygame.K_UP and hero_coordinates[3] == True:
-            hero_coordinates[1] += 1
+        # опускается на один этаж, меняется координата по z
+        elif event.key == pygame.K_DOWN and hero_coordinates[2] != 1 and hero_coordinates[3]:
+            hero_coordinates[2] -= 1
+        # поднимается на один этаж, меняется координата по z
+        elif event.key == pygame.K_UP and hero_coordinates[2] != 5 and hero_coordinates[3]:
+            hero_coordinates[2] += 1
     return game_end, hero_coordinates
-
