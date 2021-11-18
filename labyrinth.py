ff62dfad5ab4b29678e500476772f6bb4c898169
Kazimirov-Yaroslav <91326850+Kazimirov-Yaroslav@users.noreply.github.com>
2021-11-18 13:44:04 +0300

    Лабиринт
    Сделал более удобное создание новых лабиринтов, добавил функцию возврата  комнатыиз лабиринта и возврат этажа

--- labyrinth.py
+++ labyrinth.py
@@ -1,39 +1,92 @@
-from enum import Enum # как в лекции 11 числа по программированию
-import numpy as np
-import game_field
+
+def input_labyrinth(input_filename, numbers_floor):
+    floor = []
+    with open(input_filename) as input_file:
+        for line in input_file:
+            if len(line.strip()) == 0 or line[0] == '#':
+                continue  # пустые строки и строки-комментарии пропускаем
+            name = line.split()[0]
+            if name == 'size':
+                size = int(line.split()[2])
+            if name == numbers_floor:
+                axe_x = []
+                list_symbol = line.split()
+                for i in range(1, len(list_symbol)):
+                    if list_symbol[i] == '0':
+                        axe_x.append('empty')
+                    if list_symbol[i] == 'd':
+                        axe_x.append('door')
+                    if list_symbol[i] == 'e':
+                        axe_x.append('elevator')
+                    if list_symbol[i] == 'x':
+                        axe_x.append('block')
+                floor.append(axe_x)
+    return floor
 
 
-class Labyrinth(Enum):
-    def __init__(self, size_x = 5, size_y = 5, size_z = 3):
-        # Инициализация трехмерного лабиринта
-        # первая координата - ось x, вторая - y, третья - z
+def labyrinth_transform(floor, number):
+    for i in range(len(floor)):
+        for j in range(len(floor[i])):
+            if floor[i][j] == 'empty':
+                floor[i][j] = Room([j, number, i], 'empty')
+            if floor[i][j] == 'block':
+                floor[i][j] = Room([j, number, i], 'block')
+            if floor[i][j] == 'elevator':
+                floor[i][j] = Room([j, number, i], 'elevator')
+            if floor[i][j] == 'door':
+                floor[i][j] = Room([j, number, i], 'door')
+    return floor
 
 
-        # можно записать лабиринт в кодировке в файл и затем как в проекте Солнечная система считать лабиринт из файла
+class Labyrinth:
+    def __init__(self, filename):
+        """
+        Инициализация трехмерного лабиринта. Он разбит на три этажа, чтобы было удобней воспринимать. Каждый этаж -
+        двумерный массив, в котором массивы - строчки, параллельные оси Ox, в текстовом файле это прописано.
+        """
+        self.first_floor = input_labyrinth(filename, '1')
+        self.second_floor = input_labyrinth(filename, '2')
+        self.third_floor = input_labyrinth(filename, '3')
+        self.first_floor = labyrinth_transform(self.first_floor, 1)
+        self.second_floor = labyrinth_transform(self.second_floor, 2)
+        self.third_floor = labyrinth_transform(self.third_floor, 3)
 
-        self.template = np.zeros(size_x, size_y, size_z)
-        self.template =[[[game_field.Room()]*size_x for i in range(size_y)] for i in range(size_z)]
+    def get_floor(self, number):
+        if number == '1':
+            return self.first_floor
+        if number == '2':
+            return self.second_floor
+        if number == '3':
+            return self.third_floor
 
     def get_room(self, x, y, z):
         """
-        возвращает объект Room с заданными координатами
-
-        :param x:
-        :param y:
-        :param z:
-        :return:
+        Возвращает комнату по данным координатам
         """
-        return self.template[x][y][z]
-
-# class Cells:
-#     def __init__(self):
-#         # 0 - пустая клетка
-#         # 1 - препятствие
-#         # 21 - лестница только вверх
-#         # 22 - лестница только вниз
-#         # 20 лестница вверх и вниз
-#         EMPTY = 0
-#         BARRIER = 1
-#         ELEVATOR = 2
+        if y == 1:
+            return self.first_floor[z][x]
+        if y == 2:
+            return self.second_floor[z][x].get_type
+        if y == 3:
+            return self.third_floor[z][x].get_type
 
 
+class Room:
+    def __init__(self, coordinates, type_of_room):
+        self.room_coordinates = coordinates
+        self.type = type_of_room
+
+    def get_cords(self):
+        return tuple(self.room_coordinates)
+
+    def get_type(self):
+        return self.type
+
+
+# one = Labyrinth('3')
+# print(one)
+# h = one.get_room(0, 1, 0)
+# print(h.get_type())
+# input_labyrinth('3', '1')
+# print(one.get_floor('1'))
+# labyrinth_transform(one.get_floor('1'), 1)
