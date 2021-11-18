0c26d79963e5c2bfb0cec682a9eb5eaef4a12776
GladyshTatiana <gladysh.tiu@phystech.edu>
2021-11-18 13:10:38 +0300

    добавила функцию лабиринта

--- labyrinth.py
+++ labyrinth.py
@@ -1,5 +1,6 @@
 from enum import Enum # как в лекции 11 числа по программированию
 import numpy as np
+import game_field
 
 
 class Labyrinth(Enum):
@@ -11,18 +12,28 @@ class Labyrinth(Enum):
         # можно записать лабиринт в кодировке в файл и затем как в проекте Солнечная система считать лабиринт из файла
 
         self.template = np.zeros(size_x, size_y, size_z)
-        self.template =[[[Cell]*size_x for i in range(size_y)] for i in range(size_z)]
+        self.template =[[[game_field.Room()]*size_x for i in range(size_y)] for i in range(size_z)]
 
+    def get_room(self, x, y, z):
+        """
+        возвращает объект Room с заданными координатами
 
-class Cells:
-    def __init__(self, width, height):
-        # 0 - пустая клетка
-        # 1 - препятствие
-        # 21 - лестница только вверх
-        # 22 - лестница только вниз
-        # 20 лестница вверх и вниз
-        EMPTY = 0
-        BARRIER = 1
-        ELEVATOR = 2
+        :param x:
+        :param y:
+        :param z:
+        :return:
+        """
+        return self.template[x][y][z]
+
+# class Cells:
+#     def __init__(self):
+#         # 0 - пустая клетка
+#         # 1 - препятствие
+#         # 21 - лестница только вверх
+#         # 22 - лестница только вниз
+#         # 20 лестница вверх и вниз
+#         EMPTY = 0
+#         BARRIER = 1
+#         ELEVATOR = 2
 
 
