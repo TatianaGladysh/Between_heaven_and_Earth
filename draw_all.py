711cb507915859991ca243d8c7650416c7f919a1
zhigalskiiivan <zhigalskii.ia@phystech.edu>
2021-11-21 17:57:45 +0300

    Изменил принцип отрисовки комнат. Теперь игрок перемещается по экрану, а не лабиринт

--- draw_all.py
+++ draw_all.py
@@ -3,8 +3,7 @@ import pygame
 import labyrinth
 import heroes
 
-rooms_in_line = 5
-rooms_in_column = 3
+indent = 40
 
 
 class Painter:
@@ -26,61 +25,66 @@ class Painter:
 
         self.window_width = _window_width
         self.window_height = _window_height
-        self.unit_height = self.window_height / rooms_in_line / 2
-        # нужно будет убрать масштабирование FIXME
-        self.img_scale_k = self.calculate_scale_k()
-        self.unit_width = self.get_unit_width()
-        self.unit_depth = self.unit_height * (1 / 3)
-        # self.zero_x, self.zero_y, self.zero_z = tuple(_main_hero.get_cords())
-        self.main_hero_screen_x = self.window_width // 2
-        self.main_hero_screen_y = self.window_height // 2
+        self.unit_height, self.unit_width, self.unit_depth = 0, 0, 0
+        self.zero_screen_cord_x = 0
+        self.zero_screen_cord_y = 0
+        self.img_scale_k = 0
         self.labyrinth = _labyrinth
         self.main_hero = _main_hero
         self.characters = _characters
         self.animator = Animator()
 
+    def calculate_unit_lengths(self):
+        img_surf = pygame.image.load("assets/Default_room.png")
+        img_width = img_surf.get_width()
+        img_height = img_surf.get_height()
+        k = img_width / img_height
+        labyrinth_x_len = self.labyrinth.get_x_width()
+        labyrinth_y_len = self.labyrinth.get_y_width()
+        self.unit_width = int(min((self.window_width - 2 * indent) / labyrinth_x_len,
+                                  k * (self.window_height - 2 * indent) / labyrinth_y_len))
+        self.unit_height = int(self.unit_width / k)
+        self.unit_depth = int(self.unit_height * 0.18333333333)
+
     def set_game_params(self, _labyrinth, _main_hero, _characters):
         self.labyrinth = _labyrinth
         self.main_hero = _main_hero
         self.characters = _characters
+        self.calculate_unit_lengths()
+        self.calculate_scale_k()
+        self.calculate_zero_screen_cords()
 
-    def get_unit_width(self):
-        """
-        рассчитывает единичную ширину (которую должна иметь картинка на экране)
-        :return: единичная ширина
-        """
-        img_surf = pygame.image.load("assets/Default_room.png")
-        img_width = img_surf.get_width()
-        return img_width * self.img_scale_k
+    def calculate_zero_screen_cords(self):
+        self.zero_screen_cord_x = int(
+            self.window_width / 2 - self.unit_width * self.labyrinth.get_x_width() / 2 + self.unit_width / 2)
+        self.zero_screen_cord_y = int(self.window_height / 2 - self.unit_height *
+                                      self.labyrinth.get_y_width() / 2 + self.unit_height / 2)
 
     def calculate_scale_k(self):
         """
         Рассчитыват коэффициент размера изображений
         """
         img_surf = pygame.image.load("assets/Default_room.png")
-        img_height = img_surf.get_width()
-        return self.unit_height / img_height
+        img_height = img_surf.get_height()
+        self.img_scale_k = self.unit_height / img_height
 
     def transform_game_cords_in_screen_cords(self, obj):
         """
         Пересчитывает координыты из игровых в экранные
-
         :param obj: объект
         :return: координаты объекта на экране(screen_z - проекция его координаты в глубину на плоскость экрана)
         """
-        game_obj_x, game_obj_y, game_obj_z = obj.get_cords()
         game_hero_x, game_hero_y, game_hero_z = self.main_hero.get_cords()
-        screen_x = self.main_hero_screen_x + self.unit_width * (game_obj_x - game_hero_x)
-        screen_y = self.main_hero_screen_y + self.unit_height * (game_obj_y - game_hero_y)
+        game_obj_x, game_obj_y, game_obj_z = obj.get_cords()
+        screen_x = self.zero_screen_cord_x + game_obj_x * self.unit_width
+        screen_y = self.zero_screen_cord_y + game_obj_y * self.unit_height
         screen_z = self.unit_depth * (game_obj_z - game_hero_z)
-        print(screen_x, screen_y)
         screen_y += screen_z
         return screen_x, screen_y
 
     def update_room_pic(self, room: labyrinth.Room, opacity: int):
         """
         Вызывает функцию отрисовки картинки, подавая в нее соответствующий комнате файл и прозрачность
-
         :param opacity: непрозрачность
         :param room: объект класса Room
         """
@@ -94,16 +98,15 @@ class Painter:
         """
 
         x0, y0, z0 = self.main_hero.get_cords()
-        for i in range(-2, 3):
-            for j in range(-2, 3):
+        for i in range(0, self.labyrinth.get_x_width()):
+            for j in range(0, self.labyrinth.get_y_width()):
                 opacity = 255
-                if x0 + i >= 0 and y0 + j >= 0 and z0 >= 0:
-                    # нужно добавить проверку на максимальный размер
-                    room = self.labyrinth.get_room(x0 + i, y0 + j, z0)
+                room = self.labyrinth.get_room(i, j, z0)
+                if isinstance(room, labyrinth.Room):
                     self.update_room_pic(room, opacity)
 
         for i in range(-1, 2):
-            opacity = 64
+            opacity = 100
             x0, y0, z0 = self.main_hero.get_cords()
             check_room = self.labyrinth.get_room(x0 + i, y0, z0 - 1)
             if check_room.type == "door":
@@ -121,9 +124,14 @@ class Painter:
         self.update_rooms_pics()
         self.update_heroes()
 
-    def update_heroes(self):
-        self.update_image_from_file(self.surf, self.main_hero_screen_x, self.main_hero_screen_y,
+    def update_main_hero_pic(self):
+        main_hero_screen_x = self.zero_screen_cord_x + self.main_hero.x * self.unit_width
+        main_hero_screen_y = self.zero_screen_cord_y + self.main_hero.y * self.unit_height
+        self.update_image_from_file(self.surf, main_hero_screen_x, main_hero_screen_y,
                                     self.main_hero.img_file, 255, self.img_scale_k)
+
+    def update_heroes(self):
+        self.update_main_hero_pic()
 
     def update_background_pics(self):
         """
